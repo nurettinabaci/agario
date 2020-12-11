feba45ade0d96d99be14e02f70e0602f083d9c57
Nurettin <abacinurettin@gmail.com>
2020-12-11 20:01:04 +0300

    Game dynamics are completed

--- blob.js
+++ blob.js
@@ -1,9 +1,32 @@
-function Blob(w,h,r) {
-    this.pos = createVector(w,h);
+function Blob(w, h, r) {
+    this.pos = createVector(w, h);
     this.r = r;
+    this.vel = createVector(0, 0);
 
+    this.update = function () {
+        var newVel = createVector(mouseX - width / 2, mouseY - height / 2);
+        newVel.setMag(3);
+        this.vel.lerp(newVel, 0.1);
+        this.pos.add(this.vel);
+
+    }
     this.show = function () {
         fill(255);
         ellipse(this.pos.x, this.pos.y, this.r * 2, this.r * 2);
     };
+
+
+
+    this.eats = function (other) {
+        var dist = p5.Vector.dist(this.pos, other.pos);
+        if (dist < (this.r + other.r)) {
+            var sum = PI * this.r * this.r + PI * other.r * other.r;
+            this.r = sqrt(sum / PI);
+            return true;
+        }
+        else {
+            return false;
+        }
+
+    }
 }
--- sketch.js
+++ sketch.js
@@ -1,19 +1,35 @@
 var blob;
+var zoom = 1;
 var blobs = [];
 function setup() {
     createCanvas(600, 600);
-    blob = new Blob(width / 2, height / 2, 60);
-    for (var i = 0; i < 10; i++) {
-        blobs[i] = new Blob(random(width), random(height), 16);
+    blob = new Blob(0, 0, 60);
+    for (var i = 0; i < 200; i++) {
+        var x = random(-width, width);
+        var y = random(-height, height);
+        blobs[i] = new Blob(x, y, 10);
     }
 }
 
 function draw() {
     background(0);
-    blob.show();
-    for (var i = 0; i < blobs.length; i++) {
+
+    translate(width / 2, height / 2);
+    var newZoom = 64 / blob.r;
+    zoom = lerp(zoom, newZoom, 0.1);
+    scale(zoom);
+    translate(-blob.pos.x, -blob.pos.y)
+
+
+    for (var i = blobs.length - 1; i >= 0; i--) {
         blobs[i].show();
+        if (blob.eats(blobs[i])) {
+            blobs.splice(i, 1);
+        }
     }
+
+    blob.show();
+    blob.update();
 
 }
 
